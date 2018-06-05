3f4402634b760af36376c9295c1d0881b65aa6c8
Priscilla H <priscillamhuynh@gmail.com>
2018-06-05 10:13:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@ separator= "|"
 lines= "-----------"
 
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "#{board[0]}, #{separator}, #{board[1]}, #{separator}, #{board[3]}"
+  puts "#{lines}"
+  puts "#{board[4]}, #{separator}, #{board[5]}, #{separator}, #{board[6]}"
+  puts "#{lines}"
+  puts "#{board[7]}, #{separator}, #{board[8]}, #{separator}, #{board[9]}"
 end
 display_board

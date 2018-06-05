0e1ba97939d49a95d3ff7a883f4607ea55508f70
Priscilla H <priscillamhuynh@gmail.com>
2018-06-05 10:11:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board=["   ","   ","   ","  ","   ","   ","   ","   ","   "]
+separator= "|"
+lines= "-----------"
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board
